4424ebf8443c63a4c32a35b01ab8dcdb60ee3dad
remph <lhr@disroot.org>
2024-10-03 14:43:35 +0100

    less worse OOD

--- foo.py
+++ foo.py
@@ -50,27 +50,35 @@ schema = {
 	}
 }
 
-class Reviews:
-	per_file = 5000 # constant
+class Review_Stream:
+	def __init__(self, steamid):
+		self.steamid = steamid # constant
+		self.cursor = '*' # assigned anew on each iteration
+		self.connection = requests.Session() # just to reuse the TCP connection
 
-	def getbatch(self):
+	def nextbatch(self, n_max):
 		r = self.connection.get("https://store.steampowered.com/appreviews/{:d}".format(self.steamid),
-						params={'json':1, 'num_per_page':self.per_file, 'cursor':self.cursor})
+						params={'json':1, 'num_per_page':n_max, 'cursor':self.cursor})
 		r.raise_for_status()
+
 		self.response_obj = r.json()
 		if not self.response_obj['success']:
 			raise Exception('bad response')
 		assert self.response_obj['query_summary']['num_reviews'] == len(self.response_obj['reviews'])
 		logging.debug('got {:d} reviews'.format(len(self.response_obj['reviews'])))
 
+		self.cursor = self.response_obj['cursor']
+
 		reviews = [xform_review(x) for x in self.response_obj['reviews']]
 		jsonschema.validate(reviews, schema,
 						format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
+		return reviews
 
-		self.cursor = self.response_obj['cursor']
+class Split_Reviews:
+	def getbatch(self):
+		reviews = self.steam.nextbatch(self.per_file)
+		self.total -= len(reviews)
 		self.reviews += reviews
-		self.total -= self.response_obj['query_summary']['num_reviews']
-
 
 	def writebatch(self):
 		outfilename = "{:d}.{:d}.json".format(self.steamid, self.file_i)
@@ -80,18 +88,17 @@ class Reviews:
 		json.dump(self.reviews[:writeme], open(outfilename, "w"), indent="\t")
 		del self.reviews[:writeme] # FIXME: there are duplicate reviews in the output. Does this delete enough?
 
-
-	def __init__(self, steamid):
-		self.steamid = steamid # constant
-		self.cursor = '*' # assigned anew on each iteration
-		self.reviews = [] # accumulates with each iteration
-		self.total = 0 # decrements after each iter (set after first as a special case)
-		self.file_i = 0 # incremented
-		self.connection = requests.Session() # just to reuse the TCP connection
+	def __init__(self, steamid, per_file=5000):
+		self.steamid = steamid	# constant
+		self.reviews = []	# accumulates with each iteration
+		self.total = 0	# decrements after each iter (set after first as a special case)
+		self.file_i = 0	# incremented monotonically
+		self.per_file = per_file	# constant
+		self.steam = Review_Stream(steamid)
 
 		# First request: get total_reviews also
 		self.getbatch()
-		self.total = self.response_obj['query_summary']['total_reviews'] - len(self.reviews)
+		self.total = self.steam.response_obj['query_summary']['total_reviews'] - len(self.reviews)
 
 		while self.total > 0:
 			self.getbatch()
@@ -101,7 +108,10 @@ class Reviews:
 	def __del__(self):
 		while len(self.reviews):
 			self.writebatch()
+		if self.total != 0:
+			logging.warning('more reviews than expected: {:d}'.format(-self.total))
+		logging.debug('final cursor was {}'.format(self.steam.cursor))
 
 # test
 logging.basicConfig(level=logging.DEBUG)
-Reviews(1158310)
+Split_Reviews(1158310)

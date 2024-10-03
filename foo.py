32786ae31c68191f12c5c00f4e22452719d908f1
remph <lhr@disroot.org>
2024-10-03 13:56:14 +0100

    Splits reviews into files of 5000

--- foo.py
+++ foo.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 import datetime
+import hashlib
 import json
 import jsonschema
-import hashlib
+import logging
 import requests
 
 # TODO: schema
 
-class Review:
-	# obj is a decoded json dict from the reviews array
-	def __init__(self, obj):
+# transforms steam input format review into output format review. obj is a
+# decoded json dict from the reviews array
+def xform_review(obj):
 		# TODO: in perl, an object is just syntactic sugar for a dict. Can
 		# we get a dict from the actual name table of the object itself?
-		self.obj = {
+		return {
 			'id'		: obj['recommendationid'],
 			'author'	: hashlib.sha256(obj['author']['steamid'].encode('utf-8')).hexdigest(),
 			# TODO: UTC? timestamp_updated or timestamp_created?
@@ -20,16 +21,13 @@ class Review:
 			'hours'		: obj['author']['playtime_at_review'], # TODO: check presumption
 			'content'	: obj['review'],
 			'comments'	: obj['comment_count'],
-			# TODO: source?
+			'source'	: 'steam',
 			'helpful'	: obj['votes_up'],
 			'funny'		: obj['votes_funny'],
 			'recommended' : obj['voted_up'] # apparently
 			# TODO: franchise and gameName -- are they really to be stored
 			# separately for each review?
 		}
-
-	def __str__(self):
-		return json.dumps(self.obj)
 
 schema = {
 	"type": "array",
@@ -44,6 +42,7 @@ schema = {
 			"hours":	{"type": "integer"},
 			"content":	{"type": "string"},
 			"comments":	{"type": "integer"},
+			"source":	{"type": "string", "pattern": "^steam$"},
 			"helpful":	{"type": "integer"},
 			"funny":	{"type": "integer"},
 			"recommended":	{"type": "boolean"}
@@ -51,30 +50,58 @@ schema = {
 	}
 }
 
-def steam_api_request(steamid):
-	reviews = []
-	cursor = '*'
-	n_requested = 100 # max supported by steam as of 2024-10-02
+class Reviews:
+	per_file = 5000 # constant
 
-	while True:
-		r = requests.get("https://store.steampowered.com/appreviews/{:d}".format(steamid),
-						params={'json':1, 'num_per_page':n_requested, 'cursor':cursor})
+	def getbatch(self):
+		r = self.connection.get("https://store.steampowered.com/appreviews/{:d}".format(self.steamid),
+						params={'json':1, 'num_per_page':self.per_file, 'cursor':self.cursor})
 		r.raise_for_status()
-		response_obj = r.json()
-		if not response_obj['success']:
+		self.response_obj = r.json()
+		if not self.response_obj['success']:
 			raise Exception('bad response')
+		assert self.response_obj['query_summary']['num_reviews'] == len(self.response_obj['reviews'])
+		logging.debug('got {:d} reviews'.format(len(self.response_obj['reviews'])))
 
-		reviews += [Review(x).obj for x in response_obj['reviews']]
+		reviews = [xform_review(x) for x in self.response_obj['reviews']]
+		jsonschema.validate(reviews, schema,
+						format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
 
-		n_received = response_obj['query_summary']['num_reviews']
-		assert n_received == len(response_obj['reviews'])
-		if n_received != n_requested:
-			break
-		cursor = response_obj['cursor']
+		self.cursor = self.response_obj['cursor']
+		self.reviews += reviews
+		self.total -= self.response_obj['query_summary']['num_reviews']
 
-	jsonschema.validate(reviews, schema,
-					format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
-	return reviews
+
+	def writebatch(self):
+		outfilename = "{:d}.{:d}.json".format(self.steamid, self.file_i)
+		self.file_i += 1 # whatever happend to postincrement?
+		writeme = min(self.per_file, len(self.reviews))
+		logging.info("Writing {} reviews to {}".format(writeme, outfilename))
+		json.dump(self.reviews[:writeme], open(outfilename, "w"), indent="\t")
+		del self.reviews[:writeme] # FIXME: there are duplicate reviews in the output. Does this delete enough?
+
+
+	def __init__(self, steamid):
+		self.steamid = steamid # constant
+		self.cursor = '*' # assigned anew on each iteration
+		self.reviews = [] # accumulates with each iteration
+		self.total = 0 # decrements after each iter (set after first as a special case)
+		self.file_i = 0 # incremented
+		self.connection = requests.Session() # just to reuse the TCP connection
+
+		# First request: get total_reviews also
+		self.getbatch()
+		self.total = self.response_obj['query_summary']['total_reviews'] - len(self.reviews)
+
+		while self.total > 0:
+			self.getbatch()
+			if len(self.reviews) >= self.per_file:
+				self.writebatch()
+
+	def __del__(self):
+		while len(self.reviews):
+			self.writebatch()
 
 # test
-print(json.dumps(steam_api_request(1158310), indent="\t"))
+logging.basicConfig(level=logging.DEBUG)
+Reviews(1158310)

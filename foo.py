0747a20b74963b8871d91226fa5b7b2b5826df59
remph <lhr@disroot.org>
2024-10-10 15:02:26 +0100

    unit testing (based on a heavily adjusted cherry-pick from master)

--- foo.py
+++ foo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # vim: noexpandtab:ts=8
 from datetime import date
 import enum
@@ -127,11 +127,15 @@ class Split_Reviews:
 	def writebatch(self):
 		outfilename = "{:d}.{:d}.json".format(self.steamid, self.file_i)
 		self.file_i += 1 # whatever happend to postincrement?
+
 		writeme = min(self.per_file, len(self.reviews))
 		logging.info("Writing {} reviews to {}".format(writeme, outfilename))
 		self.reviews.sort(key=operator.itemgetter('id'))
 		self.reviews.sort(key=operator.itemgetter('date'), reverse=True)
-		json.dump(self.reviews[:writeme], open(outfilename, "wt"), indent="\t")
+
+		with open(outfilename, "wt") as outfile:
+			json.dump(self.reviews[:writeme], outfile, indent="\t")
+
 		# Validate *after* dumping, so the bad json can still be inspected
 		# after crash. Validating only up to `writeme' prevents some
 		# reviews from being validated multiple times needlessly
@@ -139,32 +143,46 @@ class Split_Reviews:
 					format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
 		del self.reviews[:writeme]
 
-	def __init__(self, steamid, date_range, per_file=5000, date_type=Review_Stream.Date_Type.CREATED):
+	def __init__(self, steamid, date_range, per_file=5000, max_files=None, date_type=Review_Stream.Date_Type.CREATED):
 		self.steamid = steamid	# constant
 		self.per_file = per_file	# constant
 		self.reviews = []	# accumulates with each iteration
 		self.total = 0	# decrements after each iter (set after first as a special case)
 		self.file_i = 0	# incremented monotonically
+		self.max_files = max_files
+		if self.max_files is not None:
+			self.max_files -= 1	# difference between 1- and 0-indexed
 		self.steam = Review_Stream(steamid, date_type, date_range)
+		self.flushed = False
 
-		# First request: get total_reviews also
+		# First request: get total_reviews also. Construction implies making a
+		# network request, but not necessarily writing
 		self.getbatch()
 		self.total = self.steam.response_obj['query_summary']['total_reviews'] - len(self.reviews)
 
-		while self.total > 0:
+	def loop(self):
+		while self.total > 0 and (self.max_files is None or self.file_i < self.max_files):
 			self.getbatch()
 			if len(self.reviews) >= self.per_file:
 				self.writebatch()
 
-	def __del__(self):
+	def end(self):
+		if self.flushed:
+			return
+
 		while len(self.reviews):
 			self.writebatch()
+		self.flushed = True
 
 		if self.total != 0:
 			logging.warning('{:d} more reviews than expected'.format(-self.total))
 
 		logging.debug('final cursor was {}'.format(self.steam.cursor))
 
+	def __del__(self):
+		self.end()
+
 # test
-logging.basicConfig(level=logging.DEBUG)
-Split_Reviews(1382330, date_range=('2023-11-18', '2024-02-12'))
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.DEBUG)
+	Split_Reviews(1382330, date_range=('2023-11-18', '2024-02-12')).loop()
---
+++ test.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python3
+# vim: noexpandtab
+import foo
+
+from copy import deepcopy
+from datetime import date, MAXYEAR
+import json
+import jsonschema
+import logging
+import os
+import tempfile
+import unittest
+
+class Test_Schema(unittest.TestCase):
+	# This is actual output directly pasted in, except the booleans
+	# which were titlecased
+	exemplar = [
+		{
+			"id": "1bf082c3e43656758c14712184096124fe82249ab5012e36098f33a0",
+			"author": "a8729ddc925805368759374f6b4f5fd323802aff9f8be314f05bc1b0",
+			"date": "2021-02-26",
+			"hours": 2792,
+			"content": "This game is the best Persona game combat-wise, its systems and combat loop is actually pretty good but you cant date the teenagers so it sucks and i hate the color red",
+			"comments": 0,
+			"source": "steam",
+			"helpful": 1,
+			"funny": 1,
+			"recommended": False
+		},
+		{
+			"id": "166172e77d50a39ee20b13e51f7d8336ea8195407a5c600073ed812e",
+			"author": "bcaba568ed29a047f9a9286ec1b483fdac0a98ddcd006498489bcadc",
+			"date": "2021-02-26",
+			"hours": 252,
+			"content": "you can make um all skateboard",
+			"comments": 0,
+			"source": "steam",
+			"helpful": 1,
+			"funny": 0,
+			"recommended": True
+		}
+	]
+
+	def test_positive(self):
+		jsonschema.validate(self.exemplar, foo.schema,
+				format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
+		self.assertTrue(True)
+
+	def assert_bad_json(self, badjson):
+		self.assertRaises(jsonschema.exceptions.ValidationError, jsonschema.validate,
+					badjson, foo.schema, format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
+
+	def test_badsource(self):
+		badsource = deepcopy(self.exemplar)
+		badsource[1]['source'] = 'Steam'
+		self.assert_bad_json(badsource)
+
+	def test_baddate(self):
+		baddate = deepcopy(self.exemplar)
+		for d in '26-01-2021', '2021/01/26', '21-01-26':
+			baddate[0]['date'] = d
+			self.assert_bad_json(baddate)
+
+	def test_noarray(self):
+		noarray = self.exemplar[1]
+		self.assert_bad_json(noarray)
+
+	def test_none(self):
+		self.assert_bad_json(None)
+
+	def test_badhex(self):
+		badhex = deepcopy(self.exemplar)
+		badhex[1]['id'] = "166172e77d50a39ee20b13e51f7d8336ea8195407a5c600073ed812g"
+		self.assert_bad_json(badhex)
+
+	def test_shorthex(self):
+		shorthex = deepcopy(self.exemplar)
+		shorthex[1]['id'] = "166172e77d50a39ee20b13e51f7d8336ea8195407a5c600073ed812"
+		self.assert_bad_json(shorthex)
+
+
+class Test_Review_Stream(unittest.TestCase):
+	def setUp(self):
+		self.n_max = 25
+		_, self.reviews = foo.Review_Stream(1382330, foo.Review_Stream.Date_Type.UPDATED, ('2023-11-18', '2024-02-12')).nextbatch(self.n_max)
+
+	def test_valid(self):
+		jsonschema.validate(self.reviews, foo.schema,
+					format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
+
+	def test_max(self):
+		self.assertTrue(len(self.reviews) <= self.n_max)
+
+
+class Test_File_Output(unittest.TestCase):
+	# This one's pretty bulky
+	def test_output(self):
+		olddir = os.getcwd()
+		tmpd = tempfile.TemporaryDirectory(delete=False)
+		os.chdir(tmpd.name)
+
+		try:
+			steamid = 1382330
+			nmax = 50
+			maxfiles = 3
+			date_range = ('2023-11-18', '2024-02-12')
+			min_date, max_date = [date.fromisoformat(d) for d in date_range]
+			splitter = foo.Split_Reviews(steamid, date_range=date_range, per_file=nmax, max_files=maxfiles)
+			splitter.loop()
+			splitter.end()
+			file_i = splitter.file_i
+			self.assertTrue(file_i <= maxfiles, '{:d} > {:d}'.format(file_i, maxfiles))
+			del splitter
+
+			ids = set()
+
+			for i in range(maxfiles):
+				curdate = date(MAXYEAR, 12, 31)
+				with open('{:d}.{:d}.json'.format(steamid, i), 'r') as f:
+					reviews = json.load(f)
+				jsonschema.validate(reviews, foo.schema,
+						format_checker=jsonschema.Draft202012Validator.FORMAT_CHECKER)
+
+				self.assertTrue(len(reviews) <= nmax
+								if i == file_i - 1
+								else len(reviews) == nmax)
+
+				for r in reviews:
+					newdate = date.fromisoformat(r['date'])
+					self.assertFalse(newdate > curdate, 'dates out of order: {}, {}'.format(newdate, curdate))
+					self.assertTrue(min_date <= newdate <= max_date, 'date out of bounds: {}'.format(newdate))
+					if newdate < curdate:
+						curdate = newdate
+
+					self.assertFalse(r['id'] in ids, msg='duplicate id')
+					ids.add(r['id'])
+		except:
+			logging.warning('Error in test, so {} not deleted; inspect and please delete yourself'
+							.format(tmpd.name))
+			raise
+
+		# only on success do we remove tmpd
+		os.chdir(olddir)
+		tmpd.cleanup()
+
+
+if __name__ == '__main__':
+	unittest.main()
